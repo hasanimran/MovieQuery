8d80a71c502faf1c1c67f601c7c84c88cc3f6bb0
cis101 <cis101@secs.iub.edu.bd>
2017-02-06 09:34:05 +0600

    Assignment 5

--- movieQuery.js
+++ movieQuery.js
@@ -1,7 +1,7 @@
 var movieList = [
-  { title: "Star Wars: Return of the Jedi", year: "1977", length: 125, rating: "8.4", genre: "sci-fi" },
-  { title: "The Matrix Revolutions", year: "2003", length: 129, rating: "6.7", genre: "sci-fi" },
-  { title: "Omen : The Final Conflict", year: "1981", length: 108, rating: "6.7", genre: "horror" },
+  { title: "Star Wars: Return of the Jedi", year: "1977", length: 125, rating: "1", genre: "sci-fi" },
+  { title: "The Matrix Revolutions", year: "2003", length: 129, rating: "2", genre: "sci-fi" },
+  { title: "Omen : The Final Conflict", year: "1981", length: 108, rating: "3", genre: "horror" },
   { title: "Gladiator", year: "2000", length: 155, rating: "8.5", genre: "historical" },
   { title: "The Patriot", year: "2000", length: 165, rating: "7.1", genre: "historical" },
   { title: "The Matrix Reloaded”, year: “2003", length: 138, rating: "7.2", genre: "sci-fi" }
@@ -9,19 +9,27 @@ var movieList = [
 
 function getMovieTitle( index ){
   //add code
+return movieList[index].title;
 }
 
 console.log(getMovieTitle(3));
 
 function addMovie( movie ){
   //add code
+
+  movieList.push(movie);
+
+  return JSON.stringify( movieList);
 }
 
-var newMovie = {};
+var newMovie = {title: "iBoy”, year: “2003", length: 138, rating: "7.2", genre: "sci-fi"};
 console.log(addMovie(newMovie));
 
 function movieByRating(){
   //add code
+  movieList.sort(function(a, b){return b.rating - a.rating});
+  return JSON.stringify( movieList);
+
 }
 
 console.log(movieByRating());
